feb62f1f569bbf752a5e03306c8d1e7d23413f6d
rem7i <remnura@hotmail.com>
2025-07-27 18:41:07 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: true
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
